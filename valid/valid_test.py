bf5ccc381f918f284881f1a6ce4c76550ddf285e
kevinmtian <kevinmtian@gmail.com>
2016-11-25 11:11:06 -0500

    added mapk

--- valid/valid_test.py
+++ valid/valid_test.py
@@ -39,9 +39,10 @@ def mapAtK(actual, predicted, m, k = 7):
 	"""
 	predicted: list[list[strings]], list of predicted items string, like ['ind_cco_fin_ult1', 'ind_ctju_fin_ult1', 'ind_ctpp_fin_ult1', 'ind_cder_fin_ult1', 'ind_ctma_fin_ult1', 'ind_cno_fin_ult1', 'ind_ctop_fin_ult1']
 	actual : list[list[strings]]
-	m: int number of instances
+	m: int number of customers
 	"""
 	mapk = 0.0
+	score = 0.0
 	for i in range(m):
 		pred = predicted[i]
 		act = actual[i]
