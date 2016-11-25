5d8746738d06769e5a0734b01bc502de90b0553c
kevinmtian <kevinmtian@gmail.com>
2016-11-25 10:59:51 -0500

    change

--- valid/valid_test.py
+++ valid/valid_test.py
@@ -2,38 +2,38 @@ import numpy as np
 import ml_metrics as metrics
 
 def apk(actual, predicted, k=7):
-    """
-    Computes the average precision at k = 7.
-    This function computes the average prescision at k between two lists of
-    items.
-    Parameters
-    ----------
-    actual : list
-             A list of string that are to be predicted (order doesn't matter)
-    predicted : list
-                A list of predicted elements (order does matter)
-    k : int, optional
-        The maximum number of predicted elements
-    Returns
-    -------
-    score : double
-            The average precision at k over the input lists
-    """
-    if len(predicted)>k:
-        predicted = predicted[:k]
+	"""
+	Computes the average precision at k = 7.
+	This function computes the average prescision at k between two lists of
+	items.
+	Parameters
+	----------
+	actual : list
+			 A list of string that are to be predicted (order doesn't matter)
+	predicted : list
+				A list of predicted elements (order does matter)
+	k : int, optional
+		The maximum number of predicted elements
+	Returns
+	-------
+	score : double
+			The average precision at k over the input lists
+	"""
+	if len(predicted)>k:
+		predicted = predicted[:k]
 
-    score = 0.0
-    num_hits = 0.0
+	score = 0.0
+	num_hits = 0.0
 
-    for i,p in enumerate(predicted):
-        if p in actual and p not in predicted[:i]:
-            num_hits += 1.0
-            score += num_hits / (i+1.0)
+	for i,p in enumerate(predicted):
+		if p in actual and p not in predicted[:i]:
+			num_hits += 1.0
+			score += num_hits / (i+1.0)
 
-    if not actual:
-        return 0.0
+	if not actual:
+		return 0.0
 
-    return score / min(len(actual), k)
+	return score / min(len(actual), k)
 
 def mapAtK(actual, predicted, m, k = 7):
 	"""
@@ -46,11 +46,11 @@ def mapAtK(actual, predicted, m, k = 7):
 		pred = predicted[i]
 		act = actual[i]
 		score = apk(act, pred)
-        mapk += score
+		mapk += score
 
-    if m > 0:
-        mapk /= m
-    return mapk
+	if m > 0:
+		mapk /= m
+	return mapk
 
 
 
